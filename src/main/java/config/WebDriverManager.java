502160f585013210a18a7d0bb7f05538d5e8364a
Nahid Mursalzada <nahidmursalzada@gmail.com>
2026-01-21 14:40:15 +0400

    Veo

--- src/main/java/config/WebDriverManager.java
+++ src/main/java/config/WebDriverManager.java
@@ -14,8 +14,19 @@ public class WebDriverManager {
     public static WebDriver getChromeDriver(){
         if (driver==null){
             ChromeOptions options=new ChromeOptions();
+
             options.addArguments("--disable-gpu");
             options.addArguments("--disable-notification");
+
+            //CI ucun headless
+            if ("true".equalsIgnoreCase(System.getenv("HEADLESS"))) {
+                options.addArguments("--headless=new");
+                options.addArguments("--window-size=1920,1080");
+                options.addArguments("--no-sandbox");
+                options.addArguments("--disable-dev-shm-usage");
+            }
+
+
             driver=new ChromeDriver(options);
             driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
         }
